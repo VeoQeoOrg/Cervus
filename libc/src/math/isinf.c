d8da47b3443f7aceeb23ad824dcbed37a039618c
VeoQeo <qensyteam@gmail.com>
2026-02-10 18:40:45 +0300

    isnan & isinf Fixed

--- libc/src/math/isinf.c
+++ libc/src/math/isinf.c
@@ -3,13 +3,12 @@
 
 int isinf(double x) {
     union {
-        double d;
-        struct {
-            uint64_t mantissa : 52;
-            uint32_t exponent : 11;
-            uint32_t sign : 1;
-        } bits;
-    } u = { .d = x };
-    
-    return (u.bits.exponent == 0x7FF) && (u.bits.mantissa == 0);
+        double f;
+        uint64_t i;
+    } u = { .f = x };
+
+    uint64_t exp = (u.i >> 52) & 0x7FF;
+    uint64_t mantissa = u.i & 0xFFFFFFFFFFFFFULL;
+
+    return (exp == 0x7FF) && (mantissa == 0);
 }
