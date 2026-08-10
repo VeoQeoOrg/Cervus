bf958b76118f2c6968be0b8b84df4e21ca96c5f5
Wiksadeey <wiksadeey@github.com>
2026-08-11 02:18:01 +0300

    fix static ttl in ip.h

--- kernel/include/net/ip.h
+++ kernel/include/net/ip.h
@@ -9,8 +9,10 @@
 #define IPPROTO_TCP  6
 #define IPPROTO_UDP  17
 
+#define IP_DEFAULT_TTL 64
+
 void     ip_rx(netdev_t *dev, const uint8_t *pkt, size_t len);
-int      ip_send(netdev_t *dev, uint32_t dst, uint8_t proto, const void *payload, size_t len);
+int      ip_send(netdev_t *dev, uint32_t dst, uint8_t proto, const void *payload, size_t len, uint8_t ttl);
 uint16_t ip_checksum(const void *data, size_t len);
 int      loopback_drain_one(void);
 
